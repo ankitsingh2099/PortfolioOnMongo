0a4411576ec7ac96d1e76ed59256e16a8b7ff8b7
ankitsingh <ankit@ost.com>
2019-10-18 01:55:09 +0530

    final changes in add and update trades api.

--- app/services/Trade/add.js
+++ app/services/Trade/add.js
@@ -1,4 +1,4 @@
-const rootPrefix = "../../..",
+const rootPrefix = '../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base'),
   CommonValidator = require(rootPrefix + '/helpers/validators');
 
@@ -7,7 +7,7 @@ const Trade = mongoose.model('Trades');
 const User = mongoose.model('Users');
 const Stock = mongoose.model('Stocks');
 
-class Add extends ServiceBase{
+class Add extends ServiceBase {
   /**
    * Constructor to add trade.
    *
@@ -24,28 +24,28 @@ class Add extends ServiceBase{
   constructor(params) {
     super();
     const oThis = this;
-    
+
     oThis.stockSymbol = params.stock_symbol;
     oThis.userId = params.user_id;
     oThis.quantity = params.quantity;
     oThis.buyPrice = params.buy_price;
-  
+
     oThis.stockId = null;
   }
-  
+
   /**
    * Async Perform
    *
    * @returns {Promise<unknown>}
    * @private
    */
-  async _asyncPerform(){
+  async _asyncPerform() {
     const oThis = this;
-    
+
     let promiseArray = [];
-    
+
     await oThis._validateAndSanitize();
-    
+
     await oThis._fetchStockId();
 
     promiseArray.push(oThis._getUserAndUpdateObject());
@@ -54,82 +54,85 @@ class Add extends ServiceBase{
 
     await Promise.all(promiseArray);
 
-    return Promise.resolve({success: true, code: 200, result_type: 'trade_id', trade_id: oThis.tradeId})
+    return Promise.resolve({ success: true, code: 200, result_type: 'trade_id', trade_id: oThis.tradeId });
   }
-  
+
   /**
    * Validate and Sanitize
    *
    * @returns {Promise<never>}
    * @private
    */
-  async _validateAndSanitize(){
+  async _validateAndSanitize() {
     const oThis = this;
-  
-    if(!CommonValidator.validateNonNegativeNumber(oThis.userId)){
+
+    if (!CommonValidator.validateNonNegativeNumber(oThis.userId)) {
       return Promise.reject({
         error: 'param_validation_failed',
         paramter: 'user_id',
         reason: 'user id should be a number greater than 0',
         code: 422
-      })
+      });
     }
-    
-    if(!CommonValidator.validateNonNegativeNumber(oThis.quantity)){
+
+    if (!CommonValidator.validateNonNegativeNumber(oThis.quantity)) {
       return Promise.reject({
         error: 'param_validation_failed',
         parameter: 'quantity',
         reason: 'quantity should be a number greater than 0',
         code: 422
-      })
+      });
     }
     oThis.quantity = parseInt(oThis.quantity);
-    
-    if(!CommonValidator.validateNonNegativeDecimalUpto3Places(oThis.buyPrice) && !CommonValidator.validateNonNegativeInteger(oThis.buyPrice)){
+
+    if (
+      !CommonValidator.validateNonNegativeDecimalUpto3Places(oThis.buyPrice) &&
+      !CommonValidator.validateNonNegativeInteger(oThis.buyPrice)
+    ) {
       return Promise.reject({
         error: 'param_validation_failed',
         parameter: 'average_buy_price',
         reason: 'average_buy_price should be a decimal number greater than 0 and should not have decimals more than 3',
         code: 422
-      })
+      });
     }
     oThis.buyPrice = parseFloat(oThis.buyPrice).toFixed(3);
-    
-    if(!CommonValidator.validateString(oThis.stockSymbol)){
+
+    if (!CommonValidator.validateString(oThis.stockSymbol)) {
       return Promise.reject({
         error: 'param_validation_failed',
         parameter: 'stock_symbol',
         reason: 'stock_symbol should be a string',
         code: 422
-      })
+      });
     }
   }
-  
+
   /**
    * Fetch stock id.
    *
    * @returns {Promise<unknown>}
    * @private
    */
-  async _fetchStockId(){
+  async _fetchStockId() {
     const oThis = this;
-    
-    return new Promise(async function(onResolve, onReject){
-      Stock.findOne({symbol: oThis.stockSymbol}, 'symbol', {}, function(err, stock) {
-        if(err){
+
+    return new Promise(async function(onResolve, onReject) {
+      Stock.findOne({ symbol: oThis.stockSymbol }, 'symbol', {}, function(err, stock) {
+        if (err) {
           console.error(err);
-          onReject({error: 'Error while saving data', code: 500})
+          onReject({ error: 'Error while saving data', code: 500 });
         }
-        if(stock){
+        if (stock) {
           oThis.stockId = stock._id;
-          onResolve({stock: stock});
+          onResolve({ stock: stock });
         } else {
-          onReject({error: 'Invalid stock symbol', code: 422})
+          onReject({ error: 'Invalid stock symbol', code: 422 });
         }
-      })
-    })
+      });
+    });
   }
-  
+
   /**
    * Get User and Update the user's portfolio.
    *
@@ -138,28 +141,28 @@ class Add extends ServiceBase{
    */
   async _getUserAndUpdateObject() {
     const oThis = this;
-    
-    return new Promise(async function(onResolve, onReject){
-      User.findOne({user_id: oThis.userId},'user_id portfolio',{},function (err, user) {
+
+    return new Promise(async function(onResolve, onReject) {
+      User.findOne({ user_id: oThis.userId }, 'user_id portfolio', {}, function(err, user) {
         if (err) {
           console.error(err);
-          onReject({error: 'Error while saving data', code: 500})
+          onReject({ error: 'Error while saving data', code: 500 });
         }
         let existingInPortfolio = false;
-        for(let i = 0 ; i < user.portfolio.length ; i++){
-          console.log("=user.portfolio[i].stock_id==",user.portfolio[i].stock_id);
-          console.log("==oThis.stockId==",oThis.stockId);
-          if(user.portfolio[i].stock_id.toString() == oThis.stockId.toString()){
+        for (let i = 0; i < user.portfolio.length; i++) {
+          if (user.portfolio[i].stock_id.toString() == oThis.stockId.toString()) {
             existingInPortfolio = true;
             let existingQuantity = parseInt(user.portfolio[i].quantity),
               existingAvgPrice = parseFloat(user.portfolio[i].average_buy_price);
             user.portfolio[i].quantity += parseInt(oThis.quantity);
-            
-            let newAvgBuyPrice = (parseFloat(existingQuantity * existingAvgPrice) + parseFloat(oThis.buyPrice * oThis.quantity)) / (existingQuantity + oThis.quantity)
+
+            let newAvgBuyPrice =
+              (parseFloat(existingQuantity * existingAvgPrice) + parseFloat(oThis.buyPrice * oThis.quantity)) /
+              (existingQuantity + oThis.quantity);
             user.portfolio[i].average_buy_price = parseFloat(newAvgBuyPrice).toFixed(3);
           }
         }
-        if(!existingInPortfolio){
+        if (!existingInPortfolio) {
           let portfolio = {
             stock_id: oThis.stockId,
             quantity: oThis.quantity,
@@ -167,17 +170,17 @@ class Add extends ServiceBase{
           };
           user.portfolio.push(portfolio);
         }
-        User.findOneAndUpdate({user_id: oThis.userId}, user, {}, function (err, user){
+        User.findOneAndUpdate({ user_id: oThis.userId }, user, {}, function(err, user) {
           if (err) {
             console.error(err);
-            onReject({error: 'Error while updating portfolio data', code: 500})
+            onReject({ error: 'Error while updating portfolio data', code: 500 });
           }
           onResolve();
         });
       });
-    })
+    });
   }
-  
+
   /**
    * Add trade
    *
@@ -186,25 +189,25 @@ class Add extends ServiceBase{
    */
   async _addTrade() {
     const oThis = this;
-  
+
     let trade = new Trade({
       user_id: oThis.userId,
       stock_id: oThis.stockId,
       shares_quantity: oThis.quantity,
       buy_price: oThis.buyPrice
     });
-    
+
     // save model to database
-    return new Promise(async function(onResolve, onReject){
-      trade.save(function (err, trade) {
+    return new Promise(async function(onResolve, onReject) {
+      trade.save(function(err, trade) {
         if (err) {
           console.error(err);
-          onReject({error: 'Error while saving data', code: 500})
+          onReject({ error: 'Error while saving data', code: 500 });
         }
         oThis.tradeId = trade._id;
-        onResolve()
+        onResolve();
       });
-    })
+    });
   }
 }
 
--- app/services/Trade/update.js
+++ app/services/Trade/update.js
@@ -1,101 +1,230 @@
-const rootPrefix = "../../..",
+const rootPrefix = '../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base'),
   CommonValidator = require(rootPrefix + '/helpers/validators');
 
 const mongoose = require('mongoose');
 const Trade = mongoose.model('Trades');
+const User = mongoose.model('Users');
+const Stock = mongoose.model('Stocks');
 
-class Update extends ServiceBase{
+class Update extends ServiceBase {
   constructor(params) {
     super();
     const oThis = this;
-    
+
     oThis.tradeId = params.trade_id;
-    oThis.stockId = params.stock_id;
+    oThis.stockSymbol = params.stock_symbol;
     oThis.userId = params.user_id;
     oThis.quantity = params.quantity;
     oThis.buyPrice = params.buy_price;
   }
-  
+
   /**
+   * Async Perform
    *
    * @returns {Promise<unknown>}
    * @private
    */
-  async _asyncPerform(){
+  async _asyncPerform() {
     const oThis = this;
-    
+    let promiseArray1 = [];
+
     await oThis._validateAndSanitize();
-    
-    await oThis._getUserObject();
-    
-    return oThis._updateTrade();
+
+    promiseArray1.push(oThis._fetchStockId());
+
+    promiseArray1.push(oThis._fetchUserDetails());
+
+    await Promise.all(promiseArray1);
+
+    await oThis._updateTrade();
+
+    await oThis._updateUserPortfolio();
+
+    return { success: true, code: 200, tradeId: oThis.tradeId };
   }
-  
-  async _validateAndSanitize(){
+
+  /**
+   * Validate and Sanitize
+   *
+   * @returns {Promise<never>}
+   * @private
+   */
+  async _validateAndSanitize() {
     const oThis = this;
-    
-    if(!CommonValidator.validateNonNegativeNumber(oThis.quantity)){
+
+    if (!CommonValidator.validateNonNegativeNumber(oThis.userId)) {
       return Promise.reject({
+        success: false,
+        error: 'param_validation_failed',
+        paramter: 'user_id',
+        reason: 'user id should be a number greater than 0',
+        code: 422
+      });
+    }
+
+    if (!CommonValidator.validateNonNegativeNumber(oThis.quantity)) {
+      return Promise.reject({
+        success: false,
         error: 'param_validation_failed',
         paramter: 'quantity',
         reason: 'quantity should be a number greater than 0',
         code: 422
-      })
+      });
     }
-    
-    if(!CommonValidator.validateNonNegativeDecimalUpto3Places(oThis.buyPrice)){
+    oThis.quantity = parseInt(oThis.quantity);
+
+    if (
+      !CommonValidator.validateNonNegativeDecimalUpto3Places(oThis.buyPrice) &&
+      !CommonValidator.validateNonNegativeInteger(oThis.buyPrice)
+    ) {
       return Promise.reject({
+        success: false,
         error: 'param_validation_failed',
         paramter: 'average_buy_price',
         reason: 'average_buy_price should be a decimal number greater than 0 and should not have decimals more than 3',
         code: 422
-      })
+      });
+    }
+    oThis.buyPrice = parseFloat(oThis.buyPrice).toFixed(3);
+
+    if (!CommonValidator.validateString(oThis.stockSymbol)) {
+      return Promise.reject({
+        success: false,
+        error: 'param_validation_failed',
+        parameter: 'stock_symbol',
+        reason: 'stock_symbol should be a string',
+        code: 422
+      });
     }
   }
-  
-  async _getUserObject() {
-    const oThis = this;
-  }
-  
+
   /**
-   * Add trade
+   * Fetch stock id.
+   *
+   * @returns {Promise<unknown>}
+   * @private
+   */
+  async _fetchStockId() {
+    const oThis = this;
+
+    return new Promise(async function(onResolve, onReject) {
+      Stock.findOne({ symbol: oThis.stockSymbol }, 'symbol', {}, function(err, stock) {
+        if (err) {
+          console.error(err);
+          onReject({ success: false, error: 'Error while saving data', code: 500 });
+        }
+        if (stock) {
+          oThis.stockId = stock._id;
+          onResolve({ stock: stock });
+        } else {
+          onReject({ success: false, error: 'Invalid stock symbol', code: 422 });
+        }
+      });
+    });
+  }
+
+  /**
+   * Fetch user details.
+   *
+   * @returns {Promise<unknown>}
+   * @private
+   */
+  async _fetchUserDetails() {
+    const oThis = this;
+
+    return new Promise(async function(onResolve, onReject) {
+      User.findOne({ user_id: oThis.userId }, 'user_id portfolio', {}, function(err, user) {
+        if (err) {
+          console.error(err);
+          onReject({ success: false, error: 'Error while fetchin data', code: 500 });
+        }
+        if (user) {
+          oThis.user = user;
+          oThis.userPortfolio = user.portfolio;
+          onResolve();
+        } else {
+          onReject({ success: false, error: 'Invalid user id', code: 422 });
+        }
+      });
+    });
+  }
+
+  /**
+   * Update trade
    *
    * @returns {Promise<unknown>}
    * @private
    */
   async _updateTrade() {
     const oThis = this;
-    
+
     let newObject = {
       user_id: oThis.userId,
       stock_id: oThis.stockId,
       shares_quantity: oThis.quantity,
       buy_price: oThis.buyPrice
     };
-    
-    // save model to database
-    return new Promise(async function(onResolve, onReject){
-      Trade.findOneAndUpdate({_id: oThis.tradeId}, newObject, {upsert: true}, function (err, trade) {
+
+    return new Promise(async function(onResolve, onReject) {
+      Trade.findOneAndUpdate({ _id: oThis.tradeId }, newObject, { upsert: true }, function(err, trade) {
         if (err) {
           console.error(err);
-          onReject({error: 'Error while saving data', code: 500})
+          onReject({ error: 'Error while saving data', code: 500 });
         }
-        console.log(" updated trade collection.");
-        onResolve({success: true, code: 200, tradeId: trade._id})
-        
+        oThis.oldtradeSharesQuantity = parseInt(trade.shares_quantity);
+        oThis.oldTradeBuyPrice = parseFloat(trade.buy_price).toFixed(3);
+        onResolve({ success: true, code: 200, tradeId: trade._id });
       });
-    })
+    });
   }
-  
+
   /**
-   * Update portfolio
+   * Update user portfolio
    *
    * @returns {Promise<void>}
    * @private
    */
-  async _updatePortfolio(){
-  
+  async _updateUserPortfolio() {
+    const oThis = this;
+
+    let alreadyBoughtShare = false;
+    for (let i = 0; i < oThis.userPortfolio.length; i++) {
+      if (oThis.userPortfolio[i].stock_id.toString() == oThis.stockId.toString()) {
+        alreadyBoughtShare = true;
+        let totalAmountForShares =
+            parseFloat(oThis.userPortfolio[i].average_buy_price) * oThis.userPortfolio[i].quantity,
+          oldTradeSharesAmount = oThis.oldtradeSharesQuantity * oThis.oldTradeBuyPrice,
+          newTradeSharesAmount = oThis.quantity * oThis.buyPrice,
+          currentSharesQuantity = oThis.userPortfolio[i].quantity - oThis.oldtradeSharesQuantity + oThis.quantity,
+          currentAverageBuyPrice =
+            (totalAmountForShares - oldTradeSharesAmount + newTradeSharesAmount) / currentSharesQuantity;
+        oThis.userPortfolio[i].average_buy_price = parseFloat(currentAverageBuyPrice).toFixed(3);
+        oThis.userPortfolio[i].quantity = currentSharesQuantity;
+      }
+    }
+
+    if (!alreadyBoughtShare) {
+      let sharesData = {
+        quantity: oThis.quantity,
+        average_buy_price: oThis.buyPrice,
+        stock_id: oThis.stockId
+      };
+      oThis.user.portfolio.push(sharesData);
+    } else {
+      oThis.user.portfolio = oThis.userPortfolio;
+    }
+
+    return new Promise(async function(onResolve, onReject) {
+      User.findOneAndUpdate({ user_id: oThis.userId }, oThis.user, {}, function(err, user) {
+        if (err) {
+          console.error(err);
+          onReject({ error: 'Error while updating data', code: 500 });
+        }
+        console.log(' updated users portfolio.');
+        onResolve({});
+      });
+    });
   }
 }
 
