f214c61244524ba4b788a8fa276952c4dd387bd3
ankitsingh <ankit@ost.com>
2019-10-18 15:17:42 +0530

    added stock symbol in holdings data.

--- app/services/CumulativeReturns/fetch.js
+++ app/services/CumulativeReturns/fetch.js
@@ -1,53 +1,51 @@
-const rootPrefix = "../../..",
+const rootPrefix = '../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base'),
   CommonValidator = require(rootPrefix + '/helpers/validators');
 
 const mongoose = require('mongoose');
 const User = mongoose.model('Users');
 
-class Fetch extends ServiceBase{
+class Fetch extends ServiceBase {
   constructor(params) {
     super();
     const oThis = this;
-    
+
     oThis.userId = params.user_id;
   }
-  
+
   /**
    * Async Perform
    *
    * @returns {Promise<unknown>}
    * @private
    */
-  async _asyncPerform(){
+  async _asyncPerform() {
     const oThis = this;
-    
+
     await oThis._validateAndSanitize();
-    
-    //await oThis._fetchStockId();
-    
+
     return oThis._fetchReturns();
   }
-  
+
   /**
    * Validate and Sanitize
    *
    * @returns {Promise<never>}
    * @private
    */
-  async _validateAndSanitize(){
+  async _validateAndSanitize() {
     const oThis = this;
-    
-    if(!CommonValidator.validateNonNegativeNumber(oThis.userId)){
+
+    if (!CommonValidator.validateNonNegativeNumber(oThis.userId)) {
       return Promise.reject({
         error: 'param_validation_failed',
         paramter: 'user_id',
         reason: 'user id should be a number greater than 0',
         code: 422
-      })
+      });
     }
   }
-  
+
   /**
    * Fetch Holdings
    *
@@ -56,20 +54,22 @@ class Fetch extends ServiceBase{
    */
   async _fetchReturns() {
     const oThis = this;
-    
-    return new Promise(async function(onResolve, onReject){
-      User.findOne({user_id: oThis.userId},'user_id portfolio',{},function (err, user) {
+
+    return new Promise(async function(onResolve, onReject) {
+      User.findOne({ user_id: oThis.userId }, 'user_id portfolio', {}, function(err, user) {
         if (err) {
           console.error(err);
-          onReject({error: 'Error while saving data', code: 500})
+          onReject({ error: 'Error while saving data', code: 500 });
         }
         let returns = 0.0;
-        for(let i = 0 ; i < user.portfolio.length ; i++){
-          returns += ((100 - user.portfolio[i].average_buy_price) * user.portfolio[i].quantity)
+        for (let i = 0; i < user.portfolio.length; i++) {
+          let currentReturnForStock =
+            (100 - parseFloat(user.portfolio[i].average_buy_price).toFixed(3)) * user.portfolio[i].quantity;
+          returns += currentReturnForStock;
         }
-        onResolve({success: true, code: 200, result_type: 'cumulative_returns', cumulative_returns: returns})
+        onResolve({ success: true, code: 200, result_type: 'cumulative_returns', cumulative_returns: returns });
       });
-    })
+    });
   }
 }
 
--- app/services/Holdings/fetch.js
+++ app/services/Holdings/fetch.js
@@ -1,79 +1,126 @@
-const rootPrefix = "../../..",
+const rootPrefix = '../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base'),
   CommonValidator = require(rootPrefix + '/helpers/validators');
 
 const mongoose = require('mongoose');
 const User = mongoose.model('Users');
+const Stock = mongoose.model('Stocks');
 
-class Fetch extends ServiceBase{
+class Fetch extends ServiceBase {
   constructor(params) {
     super();
     const oThis = this;
-   
+
     oThis.userId = params.user_id;
+
+    oThis.stockIds = [];
+    oThis.holdingsData = [];
   }
-  
+
   /**
-   * Async Perform
+   * Async Perform.
    *
    * @returns {Promise<unknown>}
    * @private
    */
-  async _asyncPerform(){
+  async _asyncPerform() {
     const oThis = this;
-    
+
     await oThis._validateAndSanitize();
-    
-    //await oThis._fetchStockId();
-  
-    return oThis._fetchHoldings();
+
+    await oThis._fetchHoldings();
+
+    await oThis._fetchStockSymbol();
+
+    oThis._addStockSymbolInResponse();
+
+    return { success: true, code: 200, result_type: 'portfolio', portfolio: oThis.holdingsData };
   }
-  
+
   /**
-   * Validate and Sanitize
+   * Validate and Sanitize.
    *
    * @returns {Promise<never>}
    * @private
    */
-  async _validateAndSanitize(){
+  async _validateAndSanitize() {
     const oThis = this;
-    
-    if(!CommonValidator.validateNonNegativeNumber(oThis.userId)){
+
+    if (!CommonValidator.validateNonNegativeNumber(oThis.userId)) {
       return Promise.reject({
         error: 'param_validation_failed',
         paramter: 'user_id',
         reason: 'user id should be a number greater than 0',
         code: 422
-      })
+      });
     }
   }
-  
+
   /**
-   * Fetch Holdings
+   * Fetch Holdings.
    *
    * @returns {Promise<unknown>}
    * @private
    */
   async _fetchHoldings() {
     const oThis = this;
-  
-    return new Promise(async function(onResolve, onReject){
-      User.findOne({user_id: oThis.userId},'user_id portfolio',{},function (err, user) {
+
+    return new Promise(async function(onResolve, onReject) {
+      User.findOne({ user_id: oThis.userId }, 'user_id portfolio', {}, function(err, user) {
         if (err) {
           console.error(err);
-          onReject({error: 'Error while saving data', code: 500})
+          onReject({ error: 'Error while saving data', code: 500 });
         }
         let response = [];
-        for(let i = 0 ; i < user.portfolio.length ; i++){
+        for (let i = 0; i < user.portfolio.length; i++) {
           let security = {};
+          oThis.stockIds.push(mongoose.Types.ObjectId(user.portfolio[i].stock_id));
           security.stock_id = user.portfolio[i].stock_id;
           security.quantity = user.portfolio[i].quantity;
-          security.average_buy_price = user.portfolio[i].average_buy_price;
+          security.average_buy_price = parseFloat(user.portfolio[i].average_buy_price).toFixed(3);
           response.push(security);
         }
-        onResolve({success: true, code: 200, result_type: 'portfolio', portfolio: response})
+        oThis.holdingsData = response;
+        onResolve({ success: true, code: 200, result_type: 'portfolio', portfolio: response });
       });
-    })
+    });
+  }
+
+  /**
+   * Fetch stock symbol.
+   *
+   * @returns {Promise<unknown>}
+   * @private
+   */
+  async _fetchStockSymbol() {
+    const oThis = this;
+
+    return new Promise(async function(onResolve, onReject) {
+      Stock.find({ _id: { $in: oThis.stockIds } }, 'symbol', {}, function(err, stocks) {
+        if (err) {
+          console.error(err);
+          return onReject({ success: false, error: 'Error while fetching data', code: 500 });
+        }
+        oThis.stockIdToSymbolHash = {};
+        for (let i = 0; i < stocks.length; i++) {
+          oThis.stockIdToSymbolHash[stocks[i]._id] = stocks[i].symbol;
+        }
+        onResolve();
+      });
+    });
+  }
+
+  /**
+   * Add stock symbol in the holdings data hash.
+   *
+   * @private
+   */
+  _addStockSymbolInResponse() {
+    const oThis = this;
+
+    for (let i = 0; i < oThis.holdingsData.length; i++) {
+      oThis.holdingsData[i].stock_symbol = oThis.stockIdToSymbolHash[oThis.holdingsData[i].stock_id];
+    }
   }
 }
 
